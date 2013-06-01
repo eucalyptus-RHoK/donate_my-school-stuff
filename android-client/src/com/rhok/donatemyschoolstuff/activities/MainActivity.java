8cc7bbeb7b4c30e90e07af130306a0bd285fc445
erinda jaupaj <erindajaupi@gmail.com>
2013-06-01 16:32:52 +0200

    Start with the normal layouts

--- android-client/src/com/rhok/donatemyschoolstuff/activities/MainActivity.java
+++ android-client/src/com/rhok/donatemyschoolstuff/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.rhok.donatemyschoolstuff.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 import com.rhok.donatemyschoolstuff.R;
@@ -38,6 +40,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.b_search:
+			Intent intent = new Intent(this, SearchResult.class);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
