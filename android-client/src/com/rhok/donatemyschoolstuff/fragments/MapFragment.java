aa4b4cc240d66a315e021c4dfdd7854041a3be80
fxxf <giovannini.davide90@gmail.com>
2013-06-01 17:35:46 +0200

    .

--- android-client/src/com/rhok/donatemyschoolstuff/fragments/MapFragment.java
+++ android-client/src/com/rhok/donatemyschoolstuff/fragments/MapFragment.java
@@ -1,7 +1,20 @@
 package com.rhok.donatemyschoolstuff.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rhok.donatemyschoolstuff.R;
 
 public class MapFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		return inflater.inflate(R.layout.mapview_layout, container, false);
+	}
 
 }
