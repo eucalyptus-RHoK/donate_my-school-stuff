41c3504225421f98a863ef5f0e1d90553576309f
Martin Brugnara <martin.brugnara@gmail.com>
2013-06-01 18:44:43 +0200

    update models

--- backend/donate/storage/models.py
+++ backend/donate/storage/models.py
@@ -6,14 +6,42 @@ class User(models.Model):
     location_lon = models.DecimalField(max_digits=19, decimal_places=16, blank=True, null=True)
     contact = models.CharField(max_length=255, verbose_name=('principal contact'), null=False, blank=False)
 
+    class Meta(object):
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __unicode__(self):
+        return '%s {%s}' % (self.username, self.contact)
+
 class Tag(models.Model):
     value = models.CharField(max_length=255, default='', blank=False, null=False)
+
+    class Meta(object):
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+
+    def __unicode__(self):
+        return self.value or 'Invalid'
 
 class School(models.Model):
     value = models.CharField(max_length=255, default='', blank=False, null=False)
 
+    class Meta(object):
+        verbose_name = 'school'
+        verbose_name_plural = 'schools'
+
+    def __unicode__(self):
+        return self.value or 'Invalid'
+
 class Category(models.Model):
     value = models.CharField(max_length=255, default='', blank=False, null=False)
+
+    class Meta(object):
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __unicode__(self):
+        return self.value or 'Invalid'
 
 class Obj(models.Model):
     name = models.CharField(max_length=255, verbose_name=('donation name'), null=False, blank=True)
@@ -23,3 +51,9 @@ class Obj(models.Model):
     description = models.TextField(default='', null=False, blank=True)
     owner = models.ForeignKey(User, null=False)
 
+    class Meta(object):
+        verbose_name = 'object'
+        verbose_name_plural = 'objects'
+
+    def __unicode__(self):
+        return '%s [%s]' % (self.name, [str(t) for t in self.tags.all()].join(',')
