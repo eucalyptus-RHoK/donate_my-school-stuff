7c04197e55bbf8cd407aefae5c1301416b3fae04
fxxf <giovannini.davide90@gmail.com>
2013-06-01 19:27:45 +0200

    .

---
+++ android-client/src/com/rhok/donatemyschoolstuff/adapters/PhoneLayoutAdapter.java
@@ -0,0 +1,18 @@
+package com.rhok.donatemyschoolstuff.adapters;
+
+import android.content.Context;
+import android.view.View;
+
+public class PhoneLayoutAdapter extends LayoutAdapter {
+
+	public PhoneLayoutAdapter(Context ctx) {
+		super(ctx);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- android-client/src/com/rhok/donatemyschoolstuff/fragments/SearchFragment.java
+++ android-client/src/com/rhok/donatemyschoolstuff/fragments/SearchFragment.java
@@ -1,16 +1,17 @@
 package com.rhok.donatemyschoolstuff.fragments;
 
-import com.rhok.donatemyschoolstuff.R;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import com.rhok.donatemyschoolstuff.R;
 
 public class SearchFragment extends Fragment {
 
-
+	Spinner tagSpinner, schoolSpinner;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,5 +20,17 @@ public class SearchFragment extends Fragment {
 		return inflater.inflate(R.layout.search_layout, container, false);
 	}
 
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+
+		tagSpinner = (Spinner) getActivity().findViewById(R.id.tagSpinner);
+		schoolSpinner = (Spinner) getActivity()
+				.findViewById(R.id.schoolSpinner);
+
+		// TODO spinners from server
+
+	}
 
 }
