47c2b8bd03a3a6ca8dc72ada8162bcd82cb08c6e
Martin Brugnara <martin.brugnara@gmail.com>
2013-06-01 18:59:45 +0200

    rename wrong file

--- backend/donate/donate/urls.py
+++ backend/donate/donate/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'donate.views.home', name='home'),
-    url(r'^interface/', include('interface.urls')),
+    url(r'^interface/$', include('interface.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
