a2fde9aca43a8f394cee9a0714a83c63b1348e47
Martin Brugnara <martin.brugnara@gmail.com>
2013-06-01 19:01:19 +0200

    rename wrong file

--- backend/donate/donate/urls.py
+++ backend/donate/donate/urls.py
@@ -4,13 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'donate.views.home', name='home'),
-    url(r'^interface/$', include('interface.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^interface/', include('interface.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
