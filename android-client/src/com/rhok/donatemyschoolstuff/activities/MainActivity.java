19ccbe2d04a2db722cdd1ce8e8e5fa1f97dccc3d
fxxf <giovannini.davide90@gmail.com>
2013-06-01 15:48:15 +0200

    created layout for various screens size

--- android-client/src/com/rhok/donatemyschoolstuff/activities/MainActivity.java
+++ android-client/src/com/rhok/donatemyschoolstuff/activities/MainActivity.java
@@ -1,25 +1,43 @@
 package com.rhok.donatemyschoolstuff.activities;
 
-import com.rhok.donatemyschoolstuff.R;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import com.rhok.donatemyschoolstuff.R;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		TextView txt = (TextView) findViewById(R.id.test);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		int mask = (getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK);
+		switch (mask) {
+
+		case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+		case Configuration.SCREENLAYOUT_SIZE_SMALL:
+			txt.setText("small or normal");
+			super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+			break;
+		default:
+			super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+			txt.setText("large or larger");
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
