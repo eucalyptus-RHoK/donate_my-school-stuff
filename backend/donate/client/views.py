b2549ad5319e1eb107562bea89d374ad706a38e1
Martin Brugnara <martin.brugnara@gmail.com>
2013-06-02 10:36:49 +0200

    merged fronend [lorenzo]

--- backend/donate/client/views.py
+++ backend/donate/client/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render, render_to_response, redirect
+
 
 def index(request):
-    return HttpResponse('Not yet implemented')
+    return render(request, 'index.html')
