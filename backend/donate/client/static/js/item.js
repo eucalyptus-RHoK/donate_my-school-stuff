95ec156d6d2b4c1fe9f5086021e56193645e6978
Mattia Larentis <mattia@larentis.eu>
2013-06-02 15:51:11 +0200

    styled sidebar

--- backend/donate/client/static/js/item.js
+++ backend/donate/client/static/js/item.js
@@ -1,4 +1,10 @@
 (function () {
+  search = {
+    name: '',
+    category: '',
+    school: ''
+  };
+
   WelcomeView = Backbone.View.extend({
     el: "#homepage",
     initialize: function () {
@@ -17,7 +23,76 @@
       this.render();
     },
     render: function () {
-      this.$el.html(_.template($("#sidebar-template").html()));
+      var self = this
+        , url = '/interface/search/';
+      $.get(url, function (data) {
+        data = $.parseJSON(data);
+        console.log(this.el, this.$el)
+//        console.log(data)
+        self.$el.html(
+          _.template(
+            $("#sidebar-template").html(),
+            {data: data}
+          )
+        );
+      });
+    }
+  });
+
+  AddView = Backbone.View.extend({
+    el: "#homepage",
+    initialize: function () {
+      this.render();
+    },
+    events: {
+      'click button': 'submit'
+    },
+    submit: function () {
+      var base64 = $('#image-src').val().split(',')[1]
+        , image = $('#image').val().split('.');
+
+      $.post('/interface/publish/', {
+        userID: $('#userID').val(),
+        image: base64,
+        image_ext: image[image.length - 1],
+        objectName: $('#item').val(),
+        description: $('#description').val(),
+        school: $('#school').val(),
+        category: $('#category').val()
+      });
+
+      console.log('submit')
+    },
+    render: function () {
+      var self = this;
+      $('#data-panel').hide();
+      $('#homepage').show();
+
+      $.get('/interface/bootstrap/', function (data) {
+        data = $.parseJSON(data);
+        self.$el.html(_.template($("#add-template").html(), {
+          schools: data.schools,
+          categories: data.categories
+        }));
+
+        function fileOnload(e) {
+          $("#image-src").val(e.target.result);
+        }
+
+        $('#image').change(function (e) {
+          var file = e.target.files[0],
+            imageType = /image.*/;
+
+          if (!file.type.match(imageType))
+            return;
+
+          var reader = new FileReader();
+          reader.onload = fileOnload;
+          reader.readAsDataURL(file);
+        });
+
+      });
+
     }
   });
 
